109cf823f37a0eebf55daa8c0dd3a5f76cf9778c
Axel Ulmestig <axel.ulmestig@gmail.com>
2016-05-29 11:50:46 +0200

    added new pages home, cv and projects

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ var express = require('express');
 var cfenv = require('cfenv');
 
 var app = express();
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/'));
+app.set('views', __dirname + '/');
+app.engine('html', require('ejs').renderFile);
+
 var appEnv = cfenv.getAppEnv();
 
+app.get('/', function(req, res) {
+    	res.render('public/index.html');
+});
+
+app.get('/cv', function(req, res) {
+    	res.render('public/cv.html');
+});
+
+app.get('/projects', function(req, res) {
+    	res.render('public/projects.html');
+});
+
 app.listen(appEnv.port, '0.0.0.0', function() {
-  console.log("server starting on " + appEnv.url);
+  	console.log("server starting on " + appEnv.url);
 });
