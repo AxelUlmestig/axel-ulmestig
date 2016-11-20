6723c1933758141420e5daafb2dd18425b164de6
Axel Ulmestig <axel.ulmestig@gmail.com>
2016-11-20 08:42:41 +0000

    removed cfenv references and tidied up

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
 var express = require('express');
-var cfenv = require('cfenv');
 
 var app = express();
 app.use(express.static(__dirname + '/'));
 app.set('views', __dirname + '/');
 app.engine('html', require('ejs').renderFile);
 
-var appEnv = cfenv.getAppEnv();
+app.get('/', (req, res) => res.render('public/index.html'));
 
-app.get('/', function(req, res) {
-    	res.render('public/index.html');
-});
+app.get('/cv', (req, res) => res.render('public/cv.html'));
 
-app.get('/cv', function(req, res) {
-    	res.render('public/cv.html');
-});
+app.get('/projects', (req, res) => res.render('public/projects.html'));
 
-app.get('/projects', function(req, res) {
-    	res.render('public/projects.html');
-});
-
-app.listen(appEnv.port, '0.0.0.0', function() {
-  	console.log("server starting on " + appEnv.url);
-});
+var port = 1338;
+app.listen(port, () => console.log("server starting on port:", 1338));
